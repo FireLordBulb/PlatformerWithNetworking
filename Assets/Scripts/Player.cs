4b56eccd3775a80f9f222007f461ba33e34df5c3
FireLordBulb <79010333+FireLordBulb@users.noreply.github.com>
2024-08-07 12:05:57 +0200

    Made running and jumping work

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,10 @@ using Unity.Netcode;
 using UnityEngine;
 
 public class Player : NetworkBehaviour {
+    [SerializeField] private Rigidbody2D rigidBody;
     [SerializeField] private SpriteRenderer body;
+    [SerializeField] private float runningSpeed;
+    [SerializeField] private float jumpForce;
     private Vector2 currentDirection;
     public void Awake(){
         
@@ -16,10 +19,11 @@ public class Player : NetworkBehaviour {
         } else if (currentDirection.x < 0){
             body.flipX = true;
         }
-        
+        rigidBody.AddForce(new Vector2(currentDirection.x*runningSpeed, 0), ForceMode2D.Force);
     }
+    // TODO: Add holdable jump.
     public void Jump(){
-        
+        rigidBody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
     }
     public void Attack(){
         
