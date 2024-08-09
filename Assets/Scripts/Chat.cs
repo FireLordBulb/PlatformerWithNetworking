9bf2ce496662e6990cdf7689f7d191465586ce53
Vilgot Ottestig <vilgot.ottestig@gmail.com>
2024-08-09 14:01:59 +0200

    Made Chat use strings directly and verify that the server is the sender of the SendTo.Everyone Rpc

--- Assets/Scripts/Chat.cs
+++ Assets/Scripts/Chat.cs
@@ -4,55 +4,39 @@ using TMPro;
 using Unity.Collections;
 using Unity.Netcode;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class Chat : NetworkBehaviour {
     [SerializeField] private TextMeshProUGUI text;
     [SerializeField] private TMP_InputField inputField;
-    
-    private const int MaxChars128Bytes = 64;
-    private readonly NetworkVariable<bool> lastMessageIsComplete = new(true);
 
+    private Dictionary<ulong, string> usernames = new();
     private void Awake(){
         inputField.onSubmit.AddListener(message => {
-            inputField.text = message.Trim();
-            SendChatMessage(inputField.text);
+            SendChatMessage(message);
             inputField.ActivateInputField();
             inputField.text = "";
         });
         inputField.onValueChanged.AddListener(message => inputField.text = message.TrimEnd('\n').TrimEnd('\v'));
     }
     private void SendChatMessage(string message){
-        int unsentCharsInMessage = message.Length;
-        if (unsentCharsInMessage == 0){
+        message = message.Trim();
+        if (message.Length == 0){
             return;
         }
-        if (unsentCharsInMessage <= MaxChars128Bytes){
-            SubmitMessageRPC(new FixedString128Bytes(message), NetworkManager.LocalClientId);
-        } else {
-            while (MaxChars128Bytes < unsentCharsInMessage){
-                FixedString128Bytes messageChunk = new(message.Substring(message.Length - unsentCharsInMessage, MaxChars128Bytes));
-                SubmitMessageRPC(messageChunk, NetworkManager.LocalClientId);
-                unsentCharsInMessage -= MaxChars128Bytes;
-            }
-            SubmitMessageRPC(new FixedString128Bytes(message[^unsentCharsInMessage..]), NetworkManager.LocalClientId);
+        SubmitMessageRPC(message);
+    }
+    [Rpc(SendTo.Server)]
+    private void SubmitMessageRPC(string message, RpcParams rpcParams = default){
+        ulong senderClientId = rpcParams.Receive.SenderClientId;
+        if (!usernames.TryGetValue(senderClientId, out string username)){
+            username = $"{senderClientId}";
         }
-        EndMessageRPC();
-    }
-    [Rpc(SendTo.Server)]
-    private void EndMessageRPC(){
-        lastMessageIsComplete.Value = true;
-    }
-    [Rpc(SendTo.Server)]
-    private void SubmitMessageRPC(FixedString128Bytes message, ulong sender){
-        UpdateMessageRPC(message, sender);
-        lastMessageIsComplete.Value = false;
+        AddChatLineRPC($"{username}: {message}");
     }
     [Rpc(SendTo.Everyone)]
-    private void UpdateMessageRPC(FixedString128Bytes message, ulong sender){
-        if (lastMessageIsComplete.Value){
-            text.text += $"\n{sender}: ";
+    private void AddChatLineRPC(string chatLine, RpcParams rpcParams = default){
+        if (rpcParams.Receive.SenderClientId == NetworkManager.ServerClientId){
+            text.text += $"\n{chatLine}";
         }
-        text.text += message.ToString();
     }
 }
